353c6f6924580c255b304ffdbf5208541ac6b7ad
René Lázaro Collado Arteaga <renearteaga261998@gmail.com>
2025-04-21 18:57:15 -0400

    LatestAdditions

---
+++ src/components/home/LatestAdditions.tsx
@@ -0,0 +1,65 @@
+'use client';
+
+import ProductsSection from './ProductsSection';
+
+interface Product {
+  id: number;
+  name: string;
+  price: number;
+  originalPrice: number;
+  discountPercentage: number;
+  image: string;
+  freeShipping: boolean;
+}
+
+const products: Product[] = [
+  {
+    id: 1,
+    name: 'Galletas Integrales con Semillas de Chía sin cáscaras (400 g)',
+    price: 232.69,
+    originalPrice: 40.69,
+    discountPercentage: 10,
+    image: '/assets/products/img/pomos-de-agua.png',
+    freeShipping: true,
+  },
+  {
+    id: 2,
+    name: 'Galletas Integrales con Semillas de Chía sin cáscaras (400 g)',
+    price: 232.69,
+    originalPrice: 40.69,
+    discountPercentage: 10,
+    image: '/assets/products/img/especias_refinidas.png',
+    freeShipping: true,
+  },
+  {
+    id: 3,
+    name: 'Galletas Integrales con Semillas de Chía sin cáscaras (400 g)',
+    price: 232.69,
+    originalPrice: 40.69,
+    discountPercentage: 10,
+    image: '/assets/products/img/molo_para_piza.png',
+    freeShipping: true,
+  },
+  {
+    id: 4,
+    name: 'Galletas Integrales con Semillas de Chía sin cáscaras (400 g)',
+    price: 232.69,
+    originalPrice: 40.69,
+    discountPercentage: 10,
+    image: '/assets/products/img/pasta_barbicue.png',
+    freeShipping: true,
+  },
+  {
+    id: 5,
+    name: 'Galletas Integrales con Semillas de Chía sin cáscaras (400 g)',
+    price: 232.69,
+    originalPrice: 40.69,
+    discountPercentage: 10,
+    image: '/assets/products/img/rico_drato.png',
+    freeShipping: true,
+  },
+];
+
+export default function LatestAdditions() {
+  return <ProductsSection title="Últimos Añadidos" products={products} />;
+} 
