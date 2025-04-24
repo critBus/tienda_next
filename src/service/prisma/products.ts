374afe1aaf71476b7b2c668a8186c7ce8a589433
René Lázaro Collado Arteaga <renearteaga261998@gmail.com>
2025-04-24 11:39:51 -0400

    prisma include

--- src/service/prisma/products.ts
+++ src/service/prisma/products.ts
@@ -1,31 +1,13 @@
 import prisma from "@/libs/prisma";
-import { Product, ProductDTO, CategoryDTO, CompanyDTO } from "@/types";
+import { Product, ProductDTO } from "@/types";
 
 export async function getProducts(): Promise<Product[]> {
   console.log("intenta llamar a obtener los productos");
-  try {
-    const products_response: ProductDTO[] = await prisma.product.findMany();
-    const products: Product[] = [];
-    for (let i = 0; i < products_response.length; i++) {
-      const product_dto = products_response[i];
-      const company: CompanyDTO | null = await prisma.company.findUnique({
-        where: { id: product_dto.companyId },
-      });
-      const category: CategoryDTO | null = await prisma.category.findUnique({
-        where: { id: product_dto.categoryId },
-      });
-      if (company && category) {
-        products.push({
-          ...product_dto,
-          company,
-          category,
-        });
-      }
-    }
-    return products;
-  } catch (error) {
-    return Promise.reject(error);
-  }
+  const products_response: Product[] = await prisma.product.findMany({
+    include: { category: true, company: true },
+  });
+
+  return products_response;
 }
 
 export async function getProductById(id: number): Promise<ProductDTO | null> {
