de7317a5b7e452faefd9c8d008bf3a3ba22f32a8
René Lázaro Collado Arteaga <renearteaga261998@gmail.com>
2025-04-24 14:09:41 -0400

    estilos seleccionar

--- src/components/CurrencySelector.tsx
+++ src/components/CurrencySelector.tsx
@@ -23,36 +23,38 @@ export default function CurrencySelector() {
     setIsOpen(false);
   };
 
-  if (isLoading) {
-    return (
-      <div className="bg-[#F8F8F8] flex items-center justify-between w-full px-4 py-2 text-sm font-medium text-gray-700 border border-gray-300 rounded-md shadow-sm">
-        Cargando...
-      </div>
-    );
-  }
+  const iconExpand = (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      className="h-5 w-5 ml-2"
+      viewBox="0 0 20 20"
+      fill="currentColor"
+    >
+      <path
+        fillRule="evenodd"
+        d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-8-8a1 1 0 111.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
+        clipRule="evenodd"
+      />
+    </svg>
+  );
 
   return (
     <div className="relative inline-block text-left">
-      {/* Botón del selector */}
-      <button
-        type="button"
-        onClick={() => setIsOpen(!isOpen)}
-        className="bg-[#F8F8F8] flex items-center justify-between w-full px-4 py-2 text-sm font-medium text-gray-700 border border-gray-300 rounded-md shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-yellow-500"
-      >
-        {selectedCurrency?.name || "..."}
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          className="h-5 w-5 ml-2"
-          viewBox="0 0 20 20"
-          fill="currentColor"
+      {isLoading ? (
+        <div className="bg-[#F8F8F8] flex items-center justify-between w-full px-4 py-2 text-sm font-medium text-gray-700 border border-gray-300 rounded-md shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-yellow-500">
+          <span>...</span>
+          {iconExpand}
+        </div>
+      ) : (
+        <button
+          type="button"
+          onClick={() => setIsOpen(!isOpen)}
+          className="bg-[#F8F8F8] flex items-center justify-between w-full px-4 py-2 text-sm font-medium text-gray-700 border border-gray-300 rounded-md shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-yellow-500"
         >
-          <path
-            fillRule="evenodd"
-            d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-8-8a1 1 0 111.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
-            clipRule="evenodd"
-          />
-        </svg>
-      </button>
+          {selectedCurrency?.name || "..."}
+          {iconExpand}
+        </button>
+      )}
 
       {/* Menú desplegable */}
       {isOpen && (
