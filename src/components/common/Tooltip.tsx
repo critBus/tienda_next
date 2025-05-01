9b694a8f3fc1865d6815ba0d90c53a7346a3029b
René Lázaro Collado Arteaga <renearteaga261998@gmail.com>
2025-05-01 18:28:47 -0400

    triangulo tooltip

--- src/components/common/Tooltip.tsx
+++ src/components/common/Tooltip.tsx
@@ -14,6 +14,7 @@ const Tooltip = ({
       {children}
       <div className="absolute left-1/2 transform -translate-x-1/2 bottom-full mb-2 hidden group-hover:block bg-gray-800 text-white text-xs rounded py-1 px-2 z-10 whitespace-nowrap">
         {text}
+        <div className="absolute left-1/2 transform -translate-x-1/2 top-full w-0 h-0 border-l-4 border-r-4 border-t-4 border-l-transparent border-r-transparent border-t-gray-800"></div>
       </div>
     </div>
   );
