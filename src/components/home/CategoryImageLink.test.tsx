af3be3331ee8ad7fc5874af8e87f71d402ff42dc
René Lázaro Collado Arteaga <renearteaga261998@gmail.com>
2025-05-10 12:34:17 -0400

    test CategoryImageLink

--- src/components/home/CategoryImageLink.test.tsx
+++ src/components/home/CategoryImageLink.test.tsx
@@ -1,23 +1,16 @@
 import { render, screen } from "@testing-library/react";
 import CategoryImageLink from "./CategoryImageLink";
 
-jest.mock("next/image", () => (props: any) => {
-  // Renderiza una img normal para facilitar el test
-  // eslint-disable-next-line @next/next/no-img-element
-  return <img {...props} />;
-});
-
 describe("CategoryImageLink", () => {
-  it("muestra la imagen y el nombre de la categoría", () => {
+  it("muestra la imagen y el nombre de la categoría", async () => {
     const img = "/test-image.jpg";
     const name = "Categoría de Prueba";
 
     render(<CategoryImageLink img={img} name={name} />);
-
-    // Verifica que la imagen esté en el documento
     const image = screen.getByAltText(img);
     expect(image).toBeInTheDocument();
-    expect(image).toHaveAttribute("src", img);
+    const attribute = image.getAttribute("src");
+    expect(attribute).toContain(encodeURIComponent(img));
 
     // Verifica que el nombre esté en el documento
     expect(screen.getByText(name)).toBeInTheDocument();
