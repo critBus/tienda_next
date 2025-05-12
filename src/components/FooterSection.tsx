07e30f98a2e1dcdcf201aa65d5aa9397a4a55b02
René Lázaro Collado Arteaga <renearteaga261998@gmail.com>
2025-05-12 13:59:30 -0400

    FooterSection i18

--- src/components/FooterSection.tsx
+++ src/components/FooterSection.tsx
@@ -1,8 +1,11 @@
 "use client";
 
 import Image from "next/image";
+import { useTranslations } from "next-intl";
 
 export default function FooterSection() {
+  const t = useTranslations("FooterSection");
+
   return (
     <div className="bg-[#232F3F] lg:flex lg:justify-center">
       <div className="max-w-[1800px] flex flex-col text-left p-4">
@@ -15,7 +18,7 @@ export default function FooterSection() {
               +(34) 624 60 67 70
             </span>
             <span className="roboto text-[#B2B2B2] mt-2 text-base lg:roboto-bold lg:text-white">
-              Envíanos un mensaje
+              {t("contactUs")}
             </span>
             <span className="source-sans-pro text-white mt-2 text-lg lg:text-[#B2B2B2]">
               contacto@tsotienda.com
@@ -24,30 +27,30 @@ export default function FooterSection() {
 
           <div className="flex flex-col text-left lg:w-60 lg:order-first">
             <span className="roboto text-[#B2B2B2] mt-4 text-base">
-              ¿Cómo podemos ayudarte?
+              {t("howHelp")}
             </span>
             <span className="roboto-bold text-white mt-2 text-lg lg:text-1xl">
-              Ponte en contacto con nosotros en cualquier momento
+              {t("contactAnytime")}
             </span>
           </div>
           <div className="flex flex-col text-left lg:w-96">
             <span className="source-sans-pro text-white mt-4 text-lg">
-              Suscríbase a nuestro boletín
+              {t("subscribeTitle")}
             </span>
             <div className="flex flex-row mt-2 h-[48px]">
               <input
                 className="grow border-[#DF194C] bg-white border-2 rounded-l-md placeholder:pl-4 placeholder:text-[#BAB8B8] placeholder:font-['Arial']"
                 type="text"
-                placeholder="Correo"
+                placeholder={t("subscribePlaceholder")}
               />
               <button className="bg-[#DF194C] rounded-r-md heebo text-lg text-white px-4">
-                Suscribirse
+                {t("subscribeButton")}
               </button>
             </div>
           </div>
           <div className="flex flex-col text-left lg:w-60 lg:items-end">
             <span className="source-sans-pro text-[#B2B2B2] mt-4 text-lg">
-              Síguenos
+              {t("followUs")}
             </span>
             <div className="flex flex-row justify-start gap-3 mt-3">
               <Image
@@ -82,35 +85,37 @@ export default function FooterSection() {
         <div className="flex flex-col lg:flex-row lg:gap-4 lg:flex-wrap">
           <div className="flex flex-col text-left mt-2 lg:w-60">
             <span className="roboto text-[#B2B2B2] text-base">
-              Enlaces de interés
+              {t("linksTitle")}
             </span>
             <span className="source-sans-pro text-white mt-4 text-lg">
-              Inicio
+              {t("home")}
             </span>
             <span className="source-sans-pro text-white mt-1 text-lg">
-              Términos de los Servicios
+              {t("terms")}
             </span>
             <span className="source-sans-pro text-white mt-1 text-lg">
-              Conviértete en vendedor
+              {t("becomeSeller")}
             </span>
           </div>
           <div className="flex flex-col text-left mt-6 lg:w-60">
-            <span className="roboto text-[#B2B2B2] text-base">Legal</span>
+            <span className="roboto text-[#B2B2B2] text-base">
+              {t("legal")}
+            </span>
             <span className="source-sans-pro text-white mt-4 text-lg">
-              Política de envíos
+              {t("shippingPolicy")}
             </span>
             <span className="source-sans-pro text-white mt-1 text-lg">
-              Política de privacidad
+              {t("privacyPolicy")}
             </span>
             <span className="source-sans-pro text-white mt-1 text-lg">
-              Política de garantía y devoluciones
+              {t("warrantyPolicy")}
             </span>
           </div>
           <div className="flex flex-col text-left mt-6 lg:grow lg:items-end">
             <div className="lg:flex lg:flex-col lg:text-left">
               <div className="lg:w-full">
                 <span className="roboto-bold text-white text-4xl lg:text-xl">
-                  Pago seguro
+                  {t("securePayment")}
                 </span>
               </div>
 
