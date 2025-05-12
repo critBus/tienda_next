808b550e0cf2c27eefedf15d53e019d299b601b1
René Lázaro Collado Arteaga <renearteaga261998@gmail.com>
2025-05-12 13:39:33 -0400

    ShippingCuba i18

--- src/components/home/ShippingCuba.tsx
+++ src/components/home/ShippingCuba.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import InfoShipping from "./InfoShipping";
+import { useTranslations } from "next-intl";
 
 interface Info {
   icono: string;
@@ -9,37 +10,37 @@ interface Info {
   last: boolean;
 }
 
-const infos: Info[] = [
-  {
-    icono: "/assets/shipping_cuba/group.svg",
-    title: "Entrega rápida",
-    contenct:
-      "Nos aseguramos de que su entrega llegue en el menor tiempo posible.",
-    last: false,
-  },
-  {
-    icono: "/assets/shipping_cuba/bag.svg",
-    title: "Variedad de productos",
-    contenct: "Contamos con una amplia y completa variedad de productos.",
-    last: false,
-  },
-  {
-    icono: "/assets/shipping_cuba/location-tick.svg",
-    title: "Envíos a toda Cuba",
-    contenct:
-      "Contamos con servicios de entrega a lo largo y ancho de la isla.",
-    last: true,
-  },
-];
-
 export default function ShippingCuba() {
+  const t = useTranslations("ShippingCuba");
+
+  const infos: Info[] = [
+    {
+      icono: "/assets/shipping_cuba/group.svg",
+      title: t("fastDelivery"),
+      contenct: t("fastDeliveryDesc"),
+      last: false,
+    },
+    {
+      icono: "/assets/shipping_cuba/bag.svg",
+      title: t("productVariety"),
+      contenct: t("productVarietyDesc"),
+      last: false,
+    },
+    {
+      icono: "/assets/shipping_cuba/location-tick.svg",
+      title: t("shippingAllCuba"),
+      contenct: t("shippingAllCubaDesc"),
+      last: true,
+    },
+  ];
+
   return (
     <div className="flex flex-col px-2 lg:mt-6 lg:mb-16 pt-10 text-center roboto-condensed-bold">
       <h3 className="mx-2 mt-2 text-xl lg:text-3xl text-[#D09504] font-bold">
-        Más de 1500 productos en el almacén
+        {t("warehouseTitle")}
       </h3>
       <h1 className="mx-2 mt-2 mb-8 text-5xl lg:text-7xl font-bold">
-        Envíos hacia toda Cuba
+        {t("mainTitle")}
       </h1>
       <div className="lg:flex lg:justify-center lg:items-center">
         <div className="lg:flex lg:flex-row max-w-[1100px]">
