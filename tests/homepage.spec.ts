38427791bb4da007bb562af86b069df8197b2b43
René Lázaro Collado Arteaga <94305086+critBus@users.noreply.github.com>
2025-06-10 13:52:14 -0400

    quitar eslint de jest

--- tests/homepage.spec.ts
+++ tests/homepage.spec.ts
@@ -48,23 +48,9 @@ test.describe("home page", () => {
       page.getByText(messages["LatestAdditions"]["title"])
     ).toBeVisible();
   });
+
+  test("Change language", async ({ page }) => {
+    // await page.goto(getUrl("/"));
+    await page.goto("/");
+  });
 });
-
-// test("has title", async ({ page }) => {
-//   await page.goto("https://playwright.dev/");
-
-//   // Expect a title "to contain" a substring.
-//   await expect(page).toHaveTitle(/Playwright/);
-// });
-
-// test("get started link", async ({ page }) => {
-//   await page.goto("https://playwright.dev/");
-
-//   // Click the get started link.
-//   await page.getByRole("link", { name: "Get started" }).click();
-
-//   // Expects page to have a heading with the name of Installation.
-//   await expect(
-//     page.getByRole("heading", { name: "Installation" })
-//   ).toBeVisible();
-// });
