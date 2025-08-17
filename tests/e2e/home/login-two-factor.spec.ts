5177445ea7e4dfbef5fc51c38a166bf4c7103920
René Lázaro Collado Arteaga <94305086+critBus@users.noreply.github.com>
2025-08-17 12:47:30 -0400

    2fa Login failed

--- tests/e2e/home/login-two-factor.spec.ts
+++ tests/e2e/home/login-two-factor.spec.ts
@@ -42,4 +42,43 @@ test.describe("2fa Login", () => {
     // await page.waitForTimeout(5000);
     await homePage.areLoggedin();
   });
+
+  test("2fa Login failed", async ({ page }) => {
+    const loginPage = new LoginPage(page);
+
+    await loginPage.goToPage();
+    const user = await factoryUser({
+      password: "testpass",
+      emailVerified: new Date(),
+      isTwoFactorEnabled: true,
+    });
+    await loginPage.fillDataAndSubmit({
+      email: user.email,
+      password: "testpass",
+    });
+
+    const twoFactorPage = new LoginTwoFactor(page);
+    await twoFactorPage.isOnPage();
+
+    await twoFactorPage.sendCode({ code: "123456" });
+    await twoFactorPage.isMessageVisible({
+      message: "You have 1 attempt remaining.",
+    });
+
+    const twoFactorToken = await PrismaRepository.twoFactorTokenEmail.byEmail(
+      user.email
+    );
+    expect(twoFactorToken).not.toBeNull();
+
+    if (!twoFactorToken) {
+      return;
+    }
+
+    await twoFactorPage.sendCode({ code: twoFactorToken.token });
+
+    const homePage = new HomePage(page);
+    await homePage.isOnPage(); //{ locale: false }
+    // await page.waitForTimeout(5000);
+    await homePage.areLoggedin();
+  });
 });
